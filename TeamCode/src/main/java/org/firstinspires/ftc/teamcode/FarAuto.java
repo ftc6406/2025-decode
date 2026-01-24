603b6ce6e7e3ebb286d34817a43313463faf2e09
Eric Zhang <ericbkzhang@gmail.com>
2026-01-24 09:58:40 -0500

    Create skeleton for FarAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FarAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FarAuto.java
@@ -1,4 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
-public class FarAuto {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.hardwareSystems.LimiterServo;
+import org.firstinspires.ftc.teamcode.hardwareSystems.MecanumWheels;
+
+@Autonomous(name = "BlueAuto")
+public class FarAuto extends CustomLinearOp {
+
 }
